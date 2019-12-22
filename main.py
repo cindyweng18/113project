b3e0019beacddc2628740c064d6f08b1ce3023a7
Cindy <cindyweng18@gmail.com>
2019-12-22 12:51:37 -0500

    Implemented create_h function a bit

--- main.py
+++ main.py
@@ -5,14 +5,19 @@
 #
 #
 #
+import random #allows us to use randint(0,1) -> function that randomly returns a 0 or 1
 
 #Create a function to write more variations of H and L? or separate functions for H and L
 def create_h():
-    good_h = ['1','0','1','1','1','1','1','0','1','1','0','1']
+    #good_h = ['1','0','1','1','1','1','1','0','1','1','0','1']
+    for i in range(600):
+        with open("datasets.txt","a") as file:
+            file.write(str(random.randint(0,1)))
 
 #The "good" H
 with open("datasets.txt","w") as file:
-    file.write("101111101101")
+    create_h()
+    
 
 #Separates each 0 and 1 into a single element of ONE list
 with open("datasets.txt","r") as file:
