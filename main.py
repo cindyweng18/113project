fdb1d6d26aa47f94b7d1a669d8742893aa6fb8c3
Cindy <cindyweng18@gmail.com>
2019-12-22 14:18:43 -0500

    updated main
    I created functions which generates a sequence of 0s and 1s and stores it the text file

--- main.py
+++ main.py
@@ -10,17 +10,26 @@ import random #allows us to use randint(0,1) -> function that randomly returns a
 #Create a function to write more variations of H and L? or separate functions for H and L
 def create_h():
     good_h = ['1','0','1','1','1','1','1','0','1','1','0','1']
-    
-                
+    for i in range(12):
+        good_h[i] = str(random.randint(0,1))
+    return good_h
 
-#The "good" H
+def list_to_string(lst):
+    newstr = ""
+    for i in lst:
+        newstr = newstr + i
+    return newstr
+            
+                
 with open("datasets.txt","w") as file:
-    create_h()
+    for i in range(50):
+        eich = create_h()
+        file.write(list_to_string(eich))
     
 
 #Separates each 0 and 1 into a single element of ONE list
 with open("datasets.txt","r") as file:
-    line = file.readline()
+    line = file.readlines()
     print(line)
-    bits = list(line)
-    print(str(bits))
+#    bits = list(line)
+#    print(str(bits))
