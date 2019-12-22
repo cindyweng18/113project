6219ca94d6c227e4409171cbac9c49b714dd0ddc
Cindy <cindyweng18@gmail.com>
2019-12-22 10:42:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,17 @@
 #
 #
 
+#Create a function to write more variations of H and L? or separate functions for H and L
+def variations():
+
+
 #The "good" H
 with open("datasets.txt","w") as file:
-  file.write("101111101101")
+    file.write("101111101101")
+
+#Separates each 0 and 1 into a single element of ONE list
+with open("datasets.txt","r") as file:
+    line = file.readline()
+    print(line)
+    bits = list(line)
+    print(str(bits))
