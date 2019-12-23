0d7919b563edea930eecbd44bab7eb9b40a5fd59
zchoudhury1 <54909558+zchoudhury1@users.noreply.github.com>
2019-12-22 23:16:55 -0500

    Add files via upload
    the final method for creating datasets??

--- dataSetTesting.py
+++ dataSetTesting.py
@@ -4,26 +4,6 @@
 # In[1]:
 
 
-listOfH = []
-goodH = [[1,0,1,1,1,1,1,0,1,1,0,1],[1,0,1,1,0,1,1,1,1,1,0,1],[1,0,1,1,1,1,1,1,1,1,0,1],[1,0,0,1,1,1,1,0,1,1,0,1],[1,0,0,1,0,0,1,1,1,1,0,1],[1,0,0,1,1,1,1,1,1,1,0,1]]
-
-
-# In[2]:
-
-
-for i in range(len(goodH)):
-    for j in range(len(goodH[i])):
-        if (j+1)%3 != 0:
-            print(goodH[i][j],end = " ")
-        else:
-            print(goodH[i][j],end = " ")
-            print()
-    print("-----")
-
-
-# In[3]:
-
-
 def h_variant(arr,sentryPos):
     changed = 0
     new_H = arr.copy();
@@ -34,111 +14,126 @@ def h_variant(arr,sentryPos):
     return new_H
 
 
+# In[2]:
+
+
+def printList(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if (j+1)%3 != 0:
+                print(arr[i][j],end = " ")
+            else:
+                print(arr[i][j],end = " ")
+                print()
+        print("-----")
+
+
+# In[3]:
+
+
+def h_variant_list(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if arr[i][j] == 1:
+                test = h_variant(arr[i],j)
+                arr.append(test)
+    return arr
+
+
 # In[4]:
 
 
-for i in range(len(goodH)):
-    for j in range(len(goodH[i])):
-        if goodH[i][j] == 1:
-            test = h_variant(goodH[i],j)
-            listOfH.append(test)
-
-
-# In[5]:
-
-
-for i in range(len(listOfH)):
-    for j in range(len(listOfH[i])):
-        if (j+1)%3 != 0:
-            print(listOfH[i][j],end = " ")
-        else:
-            print(listOfH[i][j],end = " ")
-            print()
-    print("-----")
+def remove_duplicates (userList):
+    i = 0
+    while i < len(userList):
+        j = 1
+        while j < len(userList):
+            if userList[i] == userList[j] and i != j:
+                userList [j] = userList [len(userList) - 1]
+                userList.pop()
+                j-= 1
+            else:
+                j += 1
+        i += 1
 
 
 # In[6]:
 
 
-len(listOfH)
+def listAddBack(arr,arr2):
+    for i in range(len(arr)):
+        arr2.append(arr[i])
 
 
-# In[12]:
+# In[7]:
 
 
-for i in range(len(goodH)):
-    listOfH.remove(goodH[i])
-
-        
+def addVariants(arr):
+    remove_duplicates(arr)
+    arr = h_variant_list(arr)
+    remove_duplicates(arr)
+    return arr
 
 
 # In[8]:
 
 
+listOfH = []
+goodH = [[1,0,1,1,1,1,1,0,1,1,0,1],[1,0,1,1,0,1,1,1,1,1,0,1],[1,0,1,1,1,1,1,1,1,1,0,1],[1,0,0,1,1,1,1,0,1,1,0,1],[1,0,0,1,0,0,1,1,1,1,0,1],[1,0,0,1,1,1,1,1,1,1,0,1]]
+
+
+# In[9]:
+
+
+listOfH = goodH.copy()
+
+
+# In[12]:
+
+
+listOfH = addVariants(listOfH)
+listOfH = addVariants(listOfH)
+listOfH = addVariants(listOfH)
+
+
+# In[13]:
+
+
 len(listOfH)
 
 
-# In[14]:
+# In[15]:
 
 
-for i in range(len(listOfH)):
-    for j in range(len(listOfH[i])):
-        if listOfH[i][j] == 1:
-            test = h_variant(listOfH[i],j)
-            listOfH.append(test)
-
-
-# In[17]:
-
-
+remove_duplicates(listOfH)
 len(listOfH)
-
-
-# In[19]:
-
-
-for i in range(len(listOfH)):
-    for j in range(len(listOfH[i])):
-        if (j+1)%3 != 0:
-            print(listOfH[i][j],end = " ")
-        else:
-            print(listOfH[i][j],end = " ")
-            print()
-    print("-----")
 
 
 # In[16]:
 
 
-def remove_duplicates (userList):
-    userListLength = int (len (userList))
-    i = 0
-    i = int (i)
-    while i < userListLength:
-        j = 0
-        j = int (j)
-        while j < userListLength:
-            if userList[i] == userList[j] and i != j:
-                userList [i] = userList [userListLength - 1]
-                userList.pop()
-                userListLength -= 1
-                i-= 1
-            else:
-                j += 1
-        i += 1
-    return userList
-
-
-# In[18]:
-
-
-listOfH = remove_duplicates(listOfH)
+printList(listOfH)
 
 
 # In[ ]:
 
 
+#remove_duplicates(listOfH)
+#len(listOfH)
 
+
+# In[ ]:
+
+
+#remove_duplicates(listOfH)
+#var2 = listOfH.copy()
+
+
+# In[ ]:
+
+
+#for i in range(len(var1)):
+#   listOfH.remove(var1[i])
 
 
 # In[ ]:
