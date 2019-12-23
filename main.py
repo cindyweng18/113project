28e4ab0d237a2b994af600f2d85b5fdc9e5235f7
sajidmahmud69 <54687706+sajidmahmud69@users.noreply.github.com>
2019-12-23 02:18:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -32,3 +32,26 @@ S2 = create_S(J2,RH)
 S3 = create_S(J3,RH)
 S4 = create_S(J4,RH)
 print(S1,S2,S3,S4)
+
+
+
+
+# DELETE this if this is not what we are supposed to do
+#Training the machine
+
+
+T_1H = [0,  0,  0,  0,  0,  0,  0,  0]
+T_2H = [0,  0,  0,  0,  0,  0,  0,  0]
+T_3H = [0,  0,  0,  0,  0,  0,  0,  0]
+T_4H = [0,  0,  0,  0,  0,  0,  0,  0]
+
+allTs = [T_1H, T_2H, T_3H, T_4H]
+allSs = [S1, S2, S3, S4]
+def matchFound (Ti, Si): # this is just to increment those 7 boxes in machine learning part
+    decimalNum = bin2Numconv(Si)
+    Ti[decimalNum] += 1
+
+print ("\n\nindex:  0  1  2  3  4  5  6  7")
+for i in range (len(allTs)): # loop to repeat incrementing all T arrays
+    matchFound(allTs[i], allSs[i])
+    print ("\t"," ",allTs[i],"\t")
