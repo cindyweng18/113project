0dee7803a82d95d8e16793f0d78f623a4372d208
zchoudhury1 <54909558+zchoudhury1@users.noreply.github.com>
2019-12-22 20:01:01 -0500

    Add files via upload
    data set h testing

---
+++ Untitled.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+listOfH = []
+goodH = [[1,0,1,1,1,1,1,0,1,1,0,1],[1,0,1,1,0,1,1,1,1,1,0,1],[1,0,1,1,1,1,1,1,1,1,0,1],[1,0,0,1,1,1,1,0,1,1,0,1],[1,0,0,1,0,0,1,1,1,1,0,1],[1,0,0,1,1,1,1,1,1,1,0,1]]
+
+
+# In[2]:
+
+
+for i in range(len(goodH)):
+    for j in range(len(goodH[i])):
+        if (j+1)%3 != 0:
+            print(goodH[i][j],end = " ")
+        else:
+            print(goodH[i][j],end = " ")
+            print()
+    print("-----")
+
+
+# In[3]:
+
+
+def h_variant(arr,sentryPos):
+    changed = 0
+    new_H = arr.copy();
+    for i in range(len(new_H)):
+        if(changed == 0 and int(new_H[i]) == 1 and i>sentryPos):
+            new_H[i] = 0
+            changed = changed+1
+    return new_H
+
+
+# In[5]:
+
+
+testList = []
+for i in range(len(goodH)):
+    for j in range(len(goodH[i])):
+        if goodH[i][j] == 1:
+            test = h_variant(goodH[i],j)
+            listOfH.append(test)
+
+
+# In[8]:
+
+
+for i in range(len(listOfH)):
+    for j in range(len(listOfH[i])):
+        if (j+1)%3 != 0:
+            print(listOfH[i][j],end = " ")
+        else:
+            print(listOfH[i][j],end = " ")
+            print()
+    print("-----")
+
+
+# In[7]:
+
+
+len(listOfH)
+
