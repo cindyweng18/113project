792f67ac3f4b68504ae0f80a617352856e565a76
Cindy <cindyweng18@gmail.com>
2019-12-24 11:55:14 -0500

    Updated main.py
    Final draft of our code before attempt of bonus section. Please check as many times as possible

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-# In[1]:
+import random
+import numpy as np
 
+random.seed(84)
 
 def variant(arr,sentryPos):
     changed = 0
@@ -10,24 +12,6 @@ def variant(arr,sentryPos):
             changed = changed+1
     return new_H
 
-
-# In[2]:
-
-
-#def printList(arr):
-#    for i in range(len(arr)):
-#        for j in range(len(arr[i])):
-#            if (j+1)%3 != 0:
-#                print(arr[i][j],end = " ")
-#            else:
-#                print(arr[i][j],end = " ")
-#                print()
-#        print("-----")
-
-
-# In[3]:
-
-
 def variant_list(arr):
     for i in range(len(arr)):
         for j in range(len(arr[i])):
@@ -35,10 +19,6 @@ def variant_list(arr):
                 test = variant(arr[i],j)
                 arr.append(test)
     return arr
-
-
-# In[4]:
-
 
 def remove_duplicates (userList):
     i = 0
@@ -53,148 +33,11 @@ def remove_duplicates (userList):
                 j += 1
         i += 1
 
-
-# In[5]:
-
-
 def addVariants(arr):
     remove_duplicates(arr)
     arr = variant_list(arr)
     remove_duplicates(arr)
     return arr
-
-
-# In[6]:
-
-
-goodH = [[1,0,1,1,1,1,1,0,1,1,0,1],[1,0,1,1,0,1,1,1,1,1,0,1],[1,0,1,1,1,1,1,1,1,1,0,1],[1,0,0,1,1,1,1,0,1,1,0,1],[1,0,0,1,0,0,1,1,1,1,0,1],[1,0,0,1,1,1,1,1,1,1,0,1]]
-
-
-# In[7]:
-
-
-listOfH = goodH.copy()
-
-
-# In[8]:
-
-
-while len(listOfH)<300:
-    listOfH = addVariants(listOfH)
-
-
-# In[9]:
-
-
-goodL = [[1,0,0,1,0,0,1,0,0,1,1,1],[1,0,0,1,0,0,1,0,0,1,0,0],[0,1,0,0,1,0,0,1,0,1,1,1],[1,0,0,1,0,0,1,0,0,1,1,0],[1,0,0,1,0,0,1,0,1,1,1,1],[0,1,0,0,1,0,0,1,1,1,1,1],[0,0,1,0,0,1,0,0,1,0,0,1],[0,1,1,0,0,1,0,0,1,0,0,1],[1,0,0,1,0,0,1,1,1,1,1,1],[0,1,0,0,1,0,1,1,0,1,1,0],[0,1,0,0,1,0,1,1,1,1,1,1],[0,0,0,0,0,0,1,0,0,1,1,1],[0,1,1,0,1,1,0,1,1,0,1,1]]
-
-
-# In[10]:
-
-
-listOfL = goodL.copy()
-
-
-# In[11]:
-
-
-while len(listOfL)<300:
-    listOfL = addVariants(listOfL)
-
-
-# In[12]:
-
-
-trainingSet = []
-testingSet = []
-for i in range(200):
-    trainingSet.append(listOfH[i]) #first 200 in text file will be H the following 200 will be L
-for i in range(200):
-    trainingSet.append(listOfL[i])
-for i in range(201,301):
-    testingSet.append(listOfH[i])
-for i in range(201,301):
-    testingSet.append(listOfL[i])
-
-
-# In[13]:
-
-
-with open("data_set.txt","a+") as dataSet:
-    for i in range(len(trainingSet)):
-        for j in range(len(trainingSet[i])):
-            dataSet.write(str(trainingSet[i][j]))
-        if i < 200:
-            dataSet.write(" H\n")
-        else:
-            dataSet.write(" L\n")
-    for i in range(len(testingSet)):
-        for j in range(len(testingSet[i])):
-            dataSet.write(str(testingSet[i][j]))
-        if i < 100:
-            dataSet.write(" H\n")
-        else:
-            dataSet.write(" L\n")
-    dataSet.close
-
-
-# In[14]:
-
-
-#with open("data_set.txt","r") as dataSet:
-#    print(dataSet.read())
-#    dataSet.close()
-
-
-# In[17]:
-
-
-with open("training_set.txt","a+") as ds:
-    for i in range(len(trainingSet)):
-        for j in range(len(trainingSet[i])):
-            ds.write(str(trainingSet[i][j]))
-        if i < 200:
-            ds.write(" H\n")
-        else:
-            ds.write(" L\n")
-    ds.close()
-
-
-# In[18]:
-
-
-with open("testing_set.txt","a+") as dst:
-    for i in range(len(testingSet)):
-        for j in range(len(testingSet[i])):
-            dst.write(str(testingSet[i][j]))
-        if i < 100:
-            dst.write(" H\n")
-        else:
-            dst.write(" L\n")
-    dst.close()
-
-    
-# this is for creating a temporary merged array and a shuffled array for the testing set
-import random
-my_file = open ("training_set.txt", "r")
-newArray = []
-newShuffledArray = []
-while True:
-    a = my_file.readline()
-    a = a.replace("\r", "").replace("\n", "")
-    if a ==  "":
-        break
-    newArray.append(a)
-my_file.close()
-#print (newArray)
-newShuffledArray = random.sample (newArray, len(newArray))
-#print (newShuffledArray)
-
-
-# In[ ]:
-
-import random
-# Michal and Akash Code
 
 def create_s(small_tuple, original_list):
     S = []
@@ -224,9 +67,9 @@ def add_sumL(Si, TLs):
 
 def predict_class(total_sumH,total_sumL):
     predicted_class = ""
-    if total_sumH > total_sumL:
+    if total_sumH >= total_sumL:
         predicted_class = "H"
-    elif total_sumH < total_sumL:
+    elif total_sumH <= total_sumL:
         predicted_class = "L"
     else:
         predicted_class = "H"
@@ -238,7 +81,71 @@ def correct_class(actual_letter,predicted_letter):
     elif actual_letter != predicted_letter:
         return "False"
 
-    
+# Dataset creation # 
+goodH = [[1,0,1,1,1,1,1,0,1,1,0,1],[1,0,1,1,0,1,1,1,1,1,0,1],[1,0,1,1,1,1,1,1,1,1,0,1],[1,0,0,1,1,1,1,0,1,1,0,1],[1,0,0,1,0,0,1,1,1,1,0,1],[1,0,0,1,1,1,1,1,1,1,0,1]]
+
+listOfH = goodH.copy()
+
+while len(listOfH)<300:
+    listOfH = addVariants(listOfH)
+
+#goodL = [[1,0,0,1,0,0,1,0,0,1,1,1],[1,0,0,1,0,0,1,0,0,1,0,0],[0,1,0,0,1,0,0,1,0,1,1,1],[1,0,0,1,0,0,1,0,0,1,1,0],[1,0,0,1,0,0,1,0,1,1,1,1],[0,1,0,0,1,0,0,1,1,1,1,1],[0,0,1,0,0,1,0,0,1,0,0,1],[0,1,1,0,0,1,0,0,1,0,0,1],[1,0,0,1,0,0,1,1,1,1,1,1],[0,1,0,0,1,0,1,1,0,1,1,0],[0,1,0,0,1,0,1,1,1,1,1,1],[0,0,0,0,0,0,1,0,0,1,1,1],[0,1,1,0,1,1,0,1,1,0,1,1]]
+
+goodL = [[1,0,0,1,0,0,1,0,0,1,1,1],[1,0,0,1,0,0,1,0,0,1,0,0],[0,1,0,0,1,0,0,1,0,1,1,1],[1,0,0,1,0,0,1,0,0,1,1,0],[1,0,0,1,0,0,1,0,1,1,1,1],[0,1,0,0,1,0,0,1,1,1,1,1],[1,0,0,1,0,0,1,1,1,1,1,1],[0,1,0,0,1,0,1,1,0,1,1,0],[0,1,0,0,1,0,1,1,1,1,1,1],[0,1,1,0,1,1,0,1,1,0,1,1]]
+
+listOfL = goodL.copy()
+
+while len(listOfL)<300:
+    listOfL = addVariants(listOfL)
+
+trainingSet = []
+testingSet = []
+for i in range(200):
+    trainingSet.append(listOfH[i]) #first 200 in text file will be H the following 200 will be L
+for i in range(101,301):
+    trainingSet.append(listOfL[i])
+for i in range(201,301):
+    testingSet.append(listOfH[i])
+for i in range(100):
+    testingSet.append(listOfL[i])
+
+with open("data_set.txt","w") as dataSet:
+    for i in range(len(trainingSet)):
+        for j in range(len(trainingSet[i])):
+            dataSet.write(str(trainingSet[i][j]))
+        if i < 200:
+            dataSet.write(" H\n")
+        else:
+            dataSet.write(" L\n")
+    for i in range(len(testingSet)):
+        for j in range(len(testingSet[i])):
+            dataSet.write(str(testingSet[i][j]))
+        if i < 100:
+            dataSet.write(" H\n")
+        else:
+            dataSet.write(" L\n")
+    dataSet.close
+
+#with open("data_set.txt","r") as dataSet:
+#    print(dataSet.read())
+#    dataSet.close()
+
+
+# Create testing set #
+my_file = open ("testing_set.txt", "r")
+newShuffledArray = []
+while True:
+    a = my_file.readline()
+    a = a.replace("\r", "").replace("\n", "")
+    if a ==  "":
+        break
+    newShuffledArray.append(a)
+my_file.close()
+#print (newArray)
+newShuffledArray = random.sample (newShuffledArray, len(newShuffledArray))
+#print (newShuffledArray)
+
+# Training Section #
 J = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 random.shuffle(J)
 J1 = tuple(J[0:3])
@@ -246,16 +153,12 @@ J2 = tuple(J[3:6])
 J3 = tuple(J[6:9])
 J4 = tuple(J[9:12])
 #print(J1, J2, J3, J4)
-
+# TH's
 array_H_1 = [0,  0,  0,  0,  0,  0,  0,  0]
 array_H_2 = [0,  0,  0,  0,  0,  0,  0,  0]
 array_H_3 = [0,  0,  0,  0,  0,  0,  0,  0]
 array_H_4 = [0,  0,  0,  0,  0,  0,  0,  0]
 allTs = [array_H_1, array_H_2, array_H_3, array_H_4]
-
-# Trainings sets for Letter H
-
-#for just testing the incrementing value
 
 trainingofH=[]
 for i in range (200):
@@ -265,51 +168,48 @@ trainingofL=[]
 for i in range (200):
     trainingofL.append(listOfL[i])
     
-for i in trainingofH:  # should run through 200 training sets of H
+# S sets
+for i in trainingofH:
 
-    S1 = create_s(J1, i) # Correct me: this sends to the function the actual H array from the data set
-    S1 = create_s(J1, i) # Fix me: this sends to the function the actual H array from the data set
+    S1 = create_s(J1, i) 
     S2 = create_s(J2, i)
     S3 = create_s(J3, i)
     S4 = create_s(J4, i)
     #print(S1, S2, S3, S4)
-    allSs = [S1, S2, S3, S4]
-                                                                                                                    
+    allSs = [S1, S2, S3, S4]           
+
     match_found (allSs[0], allTs[0])
     match_found (allSs[1], allTs[1])
     match_found (allSs[2], allTs[2])
-    match_found (allSs[3], allTs[3])
-   
-for i in range (len(allTs)): # prints all of T's after the incrementation
+    match_found (allSs[3], allTs[3])  
+    
+for i in range (len(allTs)):
     print("\t", " this is for H ", allTs[i], "\t")
-
+# TLs
 array_L_1 = [0,  0,  0,  0,  0,  0,  0,  0]
 array_L_2 = [0,  0,  0,  0,  0,  0,  0,  0]
 array_L_3 = [0,  0,  0,  0,  0,  0,  0,  0]
 array_L_4 = [0,  0,  0,  0,  0,  0,  0,  0]
 allLs = [array_L_1, array_L_2, array_L_3, array_L_4]
-# Trainings sets for Letter L
 
 
-for i in trainingofL:  # should run through 200 training sets of L
+for i in trainingofL:  
 
-
-    S1 = create_s(J1, i) # Fix me: this sends to the function the actual L array from the data set
+    S1 = create_s(J1, i) 
     S2 = create_s(J2, i)
     S3 = create_s(J3, i)
     S4 = create_s(J4, i)
     #print(S1, S2, S3, S4)
-    allSs = [S1, S2, S3, S4]
-
-                                                                                                                     
+    allSs = [S1, S2, S3, S4]                                                                                                       
     match_found (allSs[0], allLs[0])
     match_found (allSs[1], allLs[1])
     match_found (allSs[2], allLs[2])
     match_found (allSs[3], allLs[3])
         
-for i in range (len(allLs)): # prints all of L's after the incrementation
+for i in range (len(allLs)): 
     print("\t", " this is for L ", allLs[i], "\t")
 
+# Testing Section #
 j = 0
 x = []
 for i in newShuffledArray:
@@ -339,14 +239,12 @@ for i in newShuffledArray:
     predicted_letter = predict_class(total_sumH, total_sumL)
     actual_letter = newShuffledArray[j][13]
     b = correct_class(actual_letter, predicted_letter)
-    print(newShuffledArray[j][0:11], "Actual Class:", actual_letter, "Predicted Class:", predicted_letter, b)
+    print(newShuffledArray[j][0:12], "Actual Class:", actual_letter, "Predicted Class:", predicted_letter, b)
     j += 1
+    # Accuracy 
     if b == "True":
         x.append(1)
     elif b == "False":
         x.append(0)
-
-import numpy as np
 average = np.mean(x)
 print(average)
-
